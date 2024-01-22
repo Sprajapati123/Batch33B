6ac5848ea079fc53d30de94aacd3d67c2c216059
sprajapati123 <sandisprajapati52@gmail.com>
2024-01-22 09:06:47 +0545

    repo

--- lib/repository/product_repository.dart
+++ lib/repository/product_repository.dart
@@ -2,12 +2,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:week_3/model/product_model.dart';
 
 class ProductRepository {
-  final instance = FirebaseFirestore.instance
-      .collection('products')
-      .withConverter(
-          fromFirestore: (snapshot, options) =>
-              ProductModel.fromJson(snapshot.data() as Map<String, dynamic>),
-          toFirestore: (ProductModel value, options) => value.toJson(),);
+  final instance =
+      FirebaseFirestore.instance.collection('products').withConverter(
+            fromFirestore: (snapshot, options) =>
+                ProductModel.fromJson(snapshot.data() as Map<String, dynamic>),
+            toFirestore: (ProductModel value, options) => value.toJson(),
+          );
 
-
+  Future<dynamic> saveData(ProductModel model) async {
+    final res = await instance.add(model);
+    return res;
+  }
 }
